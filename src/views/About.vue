69f9430fe3c121eb196e0772fcdf3ae0708c732a
frank <ramosfp88@gmail.com>
2021-08-16 16:55:14 +0800

    updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <Header/>
+  <div class="container">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Header,
+  }
+}
+</script>
