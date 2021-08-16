8f589fce907f1cf857f1a5e29e59abfb8b0d1160
frank <ramosfp88@gmail.com>
2021-08-16 17:07:07 +0800

    dist new updates

---
+++ js/about.eab63e30.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,a,n){"use strict";n.r(a);var t=n("7a23"),c=Object(t["f"])("div",{class:"container"},[Object(t["f"])("h1",null,"This is an about page")],-1);function o(e,a,n,o,r,b){var u=Object(t["w"])("Header");return Object(t["q"])(),Object(t["e"])(t["a"],null,[Object(t["h"])(u),c],64)}var r=n("0418"),b={name:"About",components:{Header:r["a"]}};b.render=o;a["default"]=b}}]);
+//# sourceMappingURL=about.eab63e30.js.map
